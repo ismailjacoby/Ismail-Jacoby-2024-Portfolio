d48040842d9f5de8f2775cd8e8356a81a76a3111
Jacoby Ismail <ismail.jacoby@gmail.com>
2024-06-13 20:51:24 +0200

    Contact working

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,7 +1,5 @@
 // Example: Basic client-side validation
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-  console.log("JS Working")
   event.preventDefault();
   this.reset();
-  console.log("And voila")
 });
